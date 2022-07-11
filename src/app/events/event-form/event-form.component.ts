db539958a4485f7bcb107fc909a2f60eef15c8a3
Dinesh <kundudinesh007@gmail.com>
2022-07-11 22:26:26 +0530

    Fixed image upload

--- src/app/events/event-form/event-form.component.ts
+++ src/app/events/event-form/event-form.component.ts
@@ -13,7 +13,7 @@ export class EventFormComponent implements OnInit {
 
   form!: FormGroup
   categories: any = ["Coding", "Bootcamp", "Webinar", "Workshop"]
-  selectedImage: File | null = null
+  selectedImage: any = ''
 
   constructor(
     private fb: FormBuilder,
@@ -47,22 +47,32 @@ export class EventFormComponent implements OnInit {
 
   onFileChanged(event: any) {
     this.selectedImage = event.target.files[0]
-    if(this.selectedImage) {
-    const file_reader = new FileReader();
-    file_reader.readAsDataURL(this.selectedImage);
-    file_reader.onload = (e: any) => {
-      if(this.form)
-        this.form.value.image = file_reader.result;
-    }
-  }
+    // if(this.selectedImage) {
+    //   const file_reader = new FileReader();
+    //   file_reader.readAsDataURL(this.selectedImage);
+    //   file_reader.onload = (e: any) => {
+    //     if(this.form)
+    //       this.form.value.image = file_reader.result;
+    //   }
+    // }
   }
 
   submit() {
+    const eventForm = new FormData()
     const fees = parseFloat(this.form.value.amount.replace(/[₹,]+/g,""))
-    this.form.value.fees = fees;
+    eventForm.append("event[title]", this.form.value.title)
+    eventForm.append("event[description]", this.form.value.description)
+    eventForm.append("event[category]", this.form.value.category)
+    eventForm.append("event[venue]", this.form.value.venue)
+    eventForm.append("event[fees]", String(fees))
+    eventForm.append("event[reg_start]", this.form.value.reg_start)
+    eventForm.append("event[reg_end]", this.form.value.reg_end)
+    eventForm.append("event[image]", this.selectedImage)
+
+    // this.form.value.fees = fees;
     // this.form.value.image = this.selectedImage
     // console.log(this.selectedImage);
-    this.eventsService.createEvent(this.form.value)
+    this.eventsService.createEvent(eventForm)
     .subscribe(
       res => {
         // console.log(res)
